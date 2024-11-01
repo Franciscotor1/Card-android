c8025e8af92e2118f1d43106be0c5b4aa365d279
franciscotor1 <franciscotor1@FranciscoTor1>
2024-11-01 11:33:10 -0600

    imc y convertidor de grados: Cambio de colores

---
+++ app/src/main/java/com/example/cardview/ActivityHola.kt
@@ -0,0 +1,11 @@
+package com.example.cardview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityHola : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hola) // Asegúrate de que exista un layout llamado "activity_hola"
+    }
+}
